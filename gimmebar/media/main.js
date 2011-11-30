009f7dd8b7a6de4f0b72bb8c9271ffc4a0207185
paul christophe <paul.christophe6@gmail.com>
2011-11-30 10:26:20 -0600

    jsonp

--- gimmebar/media/main.js
+++ gimmebar/media/main.js
@@ -1,30 +1,21 @@
 $(document).ready(function(){
-/*
 	var handle = 'captdaylight';
 	var col = 'architecture';
 	
 	getGimme(handle, col)
 	
-	
+	function foo(data){
+		console.log("hello");
+		//Loop through all the returned results
+		if (data.length != 0){
+			console.log(data);
+		}
+	}
 	function getGimme(gimme_handle, collection){
-*/
 /* 		$.getJSON('http://api.twitter.com/1/statuses/user_timeline.json?callback=?&count=200&trim_user=t&screen_name=captdaylight&page=1', */
-/*
-		$.getJSON('https://gimmebar.com/api/v0/public/assets/captaindaylight/architecture&callback=?', 
-			function(data){
-				console.log("here");
-  				//Loop through all the returned results
-  				if (data.length != 0){
-  					console.log(data);
-  				}
-			}
+		$.getJSON('https://gimmebar.com/api/v0/public/assets/funkatron/development.js?jsonp_callback=foo', 
+			foo
 		);
 	}
-*/
-
-var url = 'https://gimmebar.com/api/v0/public/assets/captaindaylight/architecture?&callback=?';
-    	$.get(url, function(data) {
-        	alert(data)
-    	});
 });
 
