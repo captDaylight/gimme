8600aae700a9b75a4b91d9e4678ef53811139b02
paul christophe <paul.christophe6@gmail.com>
2011-11-30 11:47:18 -0600

    things

--- gimmebar/media/main.js
+++ gimmebar/media/main.js
@@ -4,17 +4,16 @@ $(document).ready(function(){
 	
 	getGimme(handle, col)
 	
-	function foo(data){
-		console.log("hello");
-		//Loop through all the returned results
-		if (data.length != 0){
-			console.log(data);
-		}
-	}
 	function getGimme(gimme_handle, collection){
 /* 		$.getJSON('http://api.twitter.com/1/statuses/user_timeline.json?callback=?&count=200&trim_user=t&screen_name=captdaylight&page=1', */
-		$.getJSON('https://gimmebar.com/api/v0/public/assets/funkatron/development.js?jsonp_callback=foo', 
-			foo
+		$.getJSON('https://gimmebar.com/api/v0/public/assets/funkatron/development.js?jsonp_callback=?', 
+			function(data){
+				console.log("hello");
+				//Loop through all the returned results
+				if (data.length != 0){
+					console.log(data);
+				}
+			}
 		);
 	}
 });
